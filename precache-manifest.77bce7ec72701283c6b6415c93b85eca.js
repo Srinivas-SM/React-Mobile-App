b42ee2ffabaf2e7e7e25f29c4f104bb5c0d0f9c7
Srinivas-SM <Srinivas.SM@outlook.com>
2020-09-28 00:10:56 +0530

    Updates

---
+++ precache-manifest.77bce7ec72701283c6b6415c93b85eca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f906d99b005235ba3f90b33e63a44a6f",
+    "url": "/React-Mobile-App/index.html"
+  },
+  {
+    "revision": "c0a97aa3eee859c4e5b0",
+    "url": "/React-Mobile-App/static/css/main.1b0f22db.chunk.css"
+  },
+  {
+    "revision": "eb0eb03cab11980f978f",
+    "url": "/React-Mobile-App/static/js/2.1c1c0074.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Mobile-App/static/js/2.1c1c0074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0a97aa3eee859c4e5b0",
+    "url": "/React-Mobile-App/static/js/main.d6bcd656.chunk.js"
+  },
+  {
+    "revision": "e4d1e2f91e38429341b7",
+    "url": "/React-Mobile-App/static/js/runtime-main.e5cbe9f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Mobile-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
